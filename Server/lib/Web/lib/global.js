ca48161feba9859beabbfd00fda426baeb811b30
김로빈 <kimrobin140410@gmail.com>
2026-06-11 19:50:01 +0900

    css랑pug같은거대수정대정령

--- Server/lib/Web/lib/global.js
+++ Server/lib/Web/lib/global.js
@@ -98,15 +98,6 @@
 			if(explSize == null) return;
 			$(".expl-active").css({ 'left': Math.min(e.clientX + 5, size[0] - explSize[0] - 12), 'top': Math.min(e.clientY + 23, size[1] - explSize[1] - 12) });
 		}).trigger('resize');
-		
-		$("#quick-search-btn").on('click', function(e){
-			var v;
-			
-			if($("#quick-search-btn").hasClass("searching")) return;
-			if(v = $(".autocomp-select").html()) $("#quick-search-tf").val(v);
-			$("#quick-search-btn").addClass("searching").html($("<i>").addClass("fa fa-spin fa-spinner"));
-			location.href = "http://jjo.kr?q=" + encodeURI($("#quick-search-tf").val());
-		}).hotkey($("#quick-search-tf"), 13);
 	
 	// 계정
 		if($.cookie('lc') == "") $.cookie('lc', "ko_KR");
